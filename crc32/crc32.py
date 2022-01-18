3f5731a2dde45d6d059c5b5c915fa9a41688002d
Notexe <josh@notex.app>
2022-01-18 11:43:45 +1000

    Add little endian result to crc32

--- crc32/crc32.py
+++ crc32/crc32.py
@@ -1,7 +1,10 @@
+from tkinter import N
+from unicodedata import name
 from redbot.core import commands
 from redbot.core.bot import Red
 import discord
 import crcengine
+import codecs
 
 class CRC32(commands.Cog):
     def __init__(self, bot: Red):
@@ -17,10 +20,15 @@ class CRC32(commands.Cog):
             crc_algorithm = crcengine.new("crc32")
             crc32Result = crc_algorithm(string.encode("utf-8"))
             crc32ResultHex = hex(crc32Result)[2:].upper()
+            crc32ResultHexLittle = codecs.encode(codecs.decode(crc32ResultHex, 'hex')[::-1], 'hex').decode().upper()
             embedCRC32 = discord.Embed(title="CRC32 Hash", color=0xC60000)
-            embedCRC32.add_field(name="Decimal:", value=crc32Result, inline=False)
+            embedCRC32.add_field(
+                name="Decimal:", value=crc32Result, inline=False)
             embedCRC32.add_field(
                 name="Hexadecimal:", value=crc32ResultHex, inline=False
+            )
+            embedCRC32.add_field(
+                name="Hexadecimal (Little Endian):", value=crc32ResultHexLittle, inline=False
             )
             await ctx.reply(embed=embedCRC32)
         else:
